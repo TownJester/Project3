19d400bfcbf0dd086da45cf0a338317c0515b88a
Madalyn <madalynshea@icloud.com>
2025-04-26 18:39:29 -0700

    Add files via upload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,69 @@
 using UnityEngine;
+using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
-//    public static GameManager instance;
-//        public int Lives = 3;
+    public static GameManager instance;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject StartMenu;
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public GameObject CreditScene;
+    public GameObject GamePlay;
+
+
+    public void Awake(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
-        
+        ActivateStartMenu();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-//    public void ReduceLivesOne(){
-//       Lives--;
-//        if (Lives <= 0){
-//        }
-//}
-}
+
+
+        public void DeactivateAllStates(){
+            StartMenu.SetActive(false);
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(false);
+            CreditScene.SetActive(false);
+            GamePlay.SetActive(false);
+        }
+        public void ActivateStartMenu(){
+            DeactivateAllStates();
+            StartMenu.SetActive(true);
+        }
+        public void ActivateMainMenu(){
+            DeactivateAllStates();
+            MainMenu.SetActive(true);
+        }
+        public void ActivateSettingsMenu(){
+            DeactivateAllStates();
+            SettingsMenu.SetActive(true);
+        }
+        public void ActivateCreditScene(){
+            DeactivateAllStates();
+            CreditScene.SetActive(true);
+        }
+        public void ActivateGamePlay(){
+            DeactivateAllStates();
+            GamePlay.SetActive(true);
+        }
+        public void QuitGame(){
+           EditorApplication.ExitPlaymode();
+            Application.Quit();
+        }
+
+    }
+
 
